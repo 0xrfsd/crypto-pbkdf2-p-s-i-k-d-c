3f4f34f7510eb09d13f619999104da2b65ce2a1f
0xrfsd <64338100+0xrfsd@users.noreply.github.com>
2021-02-10 20:03:58 -0300

    V1() => parameters exported and imported
    p, s, i, k, d

--- pbkdf2.js
+++ pbkdf2.js
@@ -1,6 +1,13 @@
 const crypto = require('crypto');
 crypto.DEFAULT_ENCODING = 'hex';
-crypto.pbkdf2('secret', 'salt', 100000, 512, 'sha512', (err, derivedKey) => {
+
+let p = 'secret message'
+let s = 'salt'
+let i = 10000
+let k = 512
+let d = 'sha512'
+
+crypto.pbkdf2(p, s, i, k, d, (err, derivedKey) => {
   if (err) throw err;
   console.log(derivedKey);  // '3745e48...aa39b34'
 });
