7dc952ba74b85c93665e67d88aa7edf4917808cf
0xrfsd <64338100+0xrfsd@users.noreply.github.com>
2021-02-10 20:00:26 -0300

    V0

---
+++ pbkdf2.js
@@ -0,0 +1,6 @@
+const crypto = require('crypto');
+crypto.DEFAULT_ENCODING = 'hex';
+crypto.pbkdf2('secret', 'salt', 100000, 512, 'sha512', (err, derivedKey) => {
+  if (err) throw err;
+  console.log(derivedKey);  // '3745e48...aa39b34'
+});
